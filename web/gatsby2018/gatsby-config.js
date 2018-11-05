9633f3445300faaf54d9a818f137faac125c21f0
Rob Lang <brainwiped@gmail.com>
2018-11-05 21:35:20 +0000

    Added remark, remove image sharp and images

--- web/gatsby2018/gatsby-config.js
+++ web/gatsby2018/gatsby-config.js
@@ -1,18 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Icar - The Free Science Fiction Tabletop RPG',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -22,9 +19,10 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-react-helmet',
+    `gatsby-transformer-remark`,
     'gatsby-plugin-offline',
   ],
 }
