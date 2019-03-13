e4449d15f38d1cd8022ac5e13274f0979190ec52
Rob Lang <brainwiped@gmail.com>
2019-03-13 21:45:29 +0000

    Added boilerplate what is Icar test and image

--- web/gatsby2019/src/pages/index.js
+++ web/gatsby2019/src/pages/index.js
@@ -5,6 +5,7 @@ import IcarTitle from "../img/front-splash-text.svg"
 import CoreRules from "../img/icar-core-rules.jpg"
 import PlayerHandouts from "../img/icar-equipment-index.jpg"
 import FleetSetting from "../img/icar-fleet-setting.jpg"
+import RulesPages from "../img/icar-core-pages.png"
 
 export default () => (
     <Layout>
@@ -44,7 +45,7 @@ export default () => (
                             <img src={CoreRules} />
                         </a>
                         <h3 className="content-subhead">
-                            <a href="https://www.drivethrurpg.com/product/133597/Icar">Core Rules</a>
+                            <a href="https://www.drivethrurpg.com/product/133597/Icar" rel="external" rev="download/version4">Core Rules</a>
                         </h3>
                         <p>
                             Everything you need to play; rules, background and setting. 96 pages. 
@@ -77,16 +78,19 @@ export default () => (
 
             <div className="ribbon l-box-lrg pure-g">
                 <div className="l-box-lrg is-center pure-u-1 pure-u-md-1-2 pure-u-lg-2-5">
-                    <img width="300" alt="File Icons" className="pure-img-responsive" src="img/common/file-icons.png" />
+                    <a href="https://www.drivethrurpg.com/product/133597/Icar" rel="external" rev="download/version4">
+                        <img width="300" alt="The core rules are high resolution PDFs filled with pictures. Click here to find out for yourself!" title="The core rules are high resolution PDFs filled with pictures. Click here to find out for yourself!" className="pure-img-responsive" src={RulesPages} />
+                        </a>
                 </div>
                 <div className="pure-u-1 pure-u-md-1-2 pure-u-lg-3-5">
 
-                    <h2 className="content-head content-head-ribbon">Laboris nisi ut aliquip.</h2>
+                    <h2 className="content-head content-head-ribbon">What is Icar?</h2>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor.
+                        Icar is a free tabletop roleplaying game, the kind you place with pencils, paper, dice and a bunch of friends. You can <a href="http://rpg.drivethrustuff.com/product/133597/Icar" rel="external" rev="download/version4">download the PDF for free</a> and either print it or play from a laptop or iPad.<br/>
+                        <br/>
+                        Icar is a space opera, more light hearted than hard science fiction. It is set in a distant future where a benevolent Imperium protect the human race, which is battling race of killer robots it created!<br/>
+                        <br/>
+                        Up to date news can be found on THE BLOG XXXXX, questions, feedback and chat can be found on <a href="http://www.1km1kt.net/forum/viewforum.php?f=34">the forum</a>.<br/>
                     </p>
                 </div>
             </div>
