d56d8d05fc8d2085d0d596b401b11f65f2370ca0
Rob Lang <brainwiped@gmail.com>
2019-05-13 21:53:20 +0100

    First draft of teams

--- web/gatsby2019/gatsby-node.js
+++ web/gatsby2019/gatsby-node.js
@@ -17,7 +17,6 @@ exports.createPages = ({ graphql, actions }) => {
     `
   ).then(result => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        console.log("also " + node.fields.slug);
         createPage({
             path: node.fields.slug,
             component: path.resolve(`./src/templates/blog.js`),
--- web/gatsby2019/src/pages/teams.js
+++ web/gatsby2019/src/pages/teams.js
@@ -6,24 +6,98 @@ export default ({ data }) => {
     return (
         <Layout>
           <div className="blog-wrapper pure-g">
-            <div className="content pure-u-1 pure-u-sm-3-4 pure-u-md-1-2">
+            <div className="content pure-u-1 pure-u-sm-3-4 pure-u-md-3-4 pure-u-lg-1-2">
             <h1>Teams</h1>
-            {data.allTeamsJson.edges.map(({ node }) => (
+            <p>
+            This page is dedicated to all those people who have played Icar with me throughout the years. They've stuck with it even through a hundred character sheet changes, seizmic rule changes and long absences.
+            </p>
+
+            <p>
+            To you, dear players, thank you.
+            </p>
+            </div>
+          </div>
+
+          {data.allTeamsJson.edges.map(({ node }) => (
+
+          <div className="team blog-wrapper pure-g">
+            <div className="content pure-u-1 pure-u-sm-3-4 pure-u-md-3-4 pure-u-lg-1-2">
+            
               <section key={node.id}>
                 <header>
                   <h2>{node.name}</h2>
-                  <p>{node.realTime.start}</p>
-               </header>
-                <div>
-                  <p></p>
-               </div>
+                </header>
+                  <p>
+                    {node.information.ooc}
+                  </p>
+                  <p>
+                    {node.information.ic}
+                  </p>
+                  
+                  <table className="pure-table">
+                    <caption hidden>Stats about the player group: dates, location and so on.</caption>
+                    <thead>
+                      <tr>
+                        <th>Dates</th>
+                        <th>In-game Dates</th>
+                        <th>GM</th>
+                        <th>Location</th>
+                        <th>Rule Version</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                          <td>
+                            {node.realTime.start} - {node.realTime.end}
+                          </td>
+                          <td>
+                            {node.imperialStandardTime.start} - {node.imperialStandardTime.end}
+                          </td>
+                          <td>
+                            {node.gamesmaster}
+                          </td>
+                          <td>
+                            {node.location}
+                          </td>
+                          <td>
+                            {node.ruleVersion}
+                          </td>
+                        </tr>
+                    </tbody>
+                  </table>
+                  <h2>Characters</h2>
+
+                  {node.playerCharacters.map(( pc, i ) => (
+                    <div key={i} className="character">
+                      <h3>{pc.name}</h3>
+                      <em>played by</em> <strong>{pc.player}</strong>
+                      <p>
+                        {pc.information}
+                      </p>
+
+                      <dl>
+                        <dt>Type</dt>
+                        <dd>{pc.type}</dd>
+                        <dt>Humanity</dt>
+                        <dd>{pc.humanity}</dd>
+                        <dt>Genus</dt>
+                        <dd>{pc.genus}</dd>
+                        <dt>Speciality</dt>
+                        <dd>{pc.speciality}</dd>
+                        <dt>Living</dt>
+                        <dd>{pc.living}</dd>
+                      </dl>
+                      <hr/>
+                    </div>
+                  ))}
+
               </section>
-              ))}
             </div>
           </div>
+
+          ))}
         </Layout>
     )
-        
 }
 
 export const query = graphql`
@@ -33,9 +107,30 @@ export const query = graphql`
             node {
                 name
                 id
+                imperialStandardTime {
+                  start
+                  end
+                }
                 realTime {
-                    start
-                    end
+                  start
+                  end
+                }
+                gamesmaster
+                location
+                ruleVersion
+                information {
+                  ooc
+                  ic
+                }
+                playerCharacters {
+                  name
+                  player
+                  type
+                  humanity
+                  genus
+                  speciality
+                  information
+                  living
                 }
             }        
         }
